44e8f1b83c8a385b4d814ae19159436e3990a482
Belikov Vyacheslav <humanunidentifed@gmail.com>
2019-08-17 20:23:53 +0300

    Fix shows-all-user-tables-size.sql

--- ms-sql/shows-all-user-tables-size.sql
+++ ms-sql/shows-all-user-tables-size.sql
@@ -1,13 +1,22 @@
-﻿-- Shows all user tables and row counts for the current database
--- Remove is_ms_shipped = 0 check to include system objects
--- i.index_id < 2 indicates clustered index (1) or hash table (0)
-
-SELECT o.NAME
-	,ddps.row_count
-FROM sys.indexes AS i
-INNER JOIN sys.objects AS o ON i.OBJECT_ID = o.OBJECT_ID
-INNER JOIN sys.dm_db_partition_stats AS ddps ON i.OBJECT_ID = ddps.OBJECT_ID
-	AND i.index_id = ddps.index_id
-WHERE i.index_id < 2
-	AND o.is_ms_shipped = 0
-ORDER BY ddps.row_count DESC
+﻿SELECT t.NAME AS TableName
+	,s.NAME AS SchemaName
+	,p.rows AS RowCounts
+	,SUM(a.total_pages) * 8 AS TotalSpaceKB
+	,CAST(ROUND(((SUM(a.total_pages) * 8) / 1024.00), 2) AS NUMERIC(36, 2)) AS TotalSpaceMB
+	,SUM(a.used_pages) * 8 AS UsedSpaceKB
+	,CAST(ROUND(((SUM(a.used_pages) * 8) / 1024.00), 2) AS NUMERIC(36, 2)) AS UsedSpaceMB
+	,(SUM(a.total_pages) - SUM(a.used_pages)) * 8 AS UnusedSpaceKB
+	,CAST(ROUND(((SUM(a.total_pages) - SUM(a.used_pages)) * 8) / 1024.00, 2) AS NUMERIC(36, 2)) AS UnusedSpaceMB
+FROM sys.tables t
+INNER JOIN sys.indexes i ON t.OBJECT_ID = i.object_id
+INNER JOIN sys.partitions p ON i.object_id = p.OBJECT_ID
+	AND i.index_id = p.index_id
+INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
+LEFT OUTER JOIN sys.schemas s ON t.schema_id = s.schema_id
+WHERE t.NAME NOT LIKE 'dt%'
+	AND t.is_ms_shipped = 0
+	AND i.OBJECT_ID > 255
+GROUP BY t.NAME
+	,s.NAME
+	,p.Rows
+ORDER BY 6 DESC
