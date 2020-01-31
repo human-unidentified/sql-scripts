86e8ff4f9711c5788853678acf5491b16f56d8ad
Belikov Vyacheslav <humanunidentifed@gmail.com>
2020-01-31 10:38:18 +0300

    Russian comment "oracle-get-db-summary information"

--- oracle/oracle-get-db-summary information.sql
+++ oracle/oracle-get-db-summary information.sql
@@ -1,24 +1,24 @@
-﻿begin
+﻿-- Сбор статистики
+begin
     dbms_stats.gather_schema_stats ('TFLEXDOCS_ORACLE');
 end;
 /
 
--- Tables + Size MB
+-- Получение размера таблиц в Мб
 select owner, table_name, round((num_rows * avg_row_len) / (1024 * 1024)) MB
 from all_tables
 where owner = 'TFLEXDOCS_ORACLE'
---and num_rows > 0  -- Ignore empty Tables.
 order by MB desc
 ;
 
---Tables + Rows
+-- Получение количества строк в таблицах
 select owner, table_name, num_rows
 from all_tables
 where owner = 'TFLEXDOCS_ORACLE'
---  and num_rows > 0          -- Ignore empty Tables.
 order by num_rows desc
 ;
 
+-- Получение метаданных о столбцах таблиц
 SELECT TABLE_NAME, COLUMN_NAME, DATA_TYPE, DATA_LENGTH
 FROM user_tab_cols
 order by TABLE_NAME, COLUMN_ID;
